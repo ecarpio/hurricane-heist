b90a3444b47b8bf53a49e6fa63ccc6c40d2833ea
Ed <ed.carpio@nafinc.com>
2018-01-18 15:24:50 -0800

    HTML framework started

--- js/function.js
+++ js/function.js
@@ -1,61 +1,8 @@
 $(function(){
 	console.log('test')
-	// Trio Height
-	// Fix Column Height
-	function fixTrioHeight(){
-		var elementHeights = $('.media-trio .col').map(function() {
-	    	return $(this).height();
-	  	}).get();
-
-		var maxHeight = Math.max.apply(null, elementHeights);
-
-		 $('.media-trio .col').height(maxHeight); 
-	}
-
-	fixTrioHeight();
-
-
-
-	// Fix Column Height
-	function fixHeight(){
-		var elementHeights = $('.testimonials .col p').map(function() {
-	    	return $(this).height();
-	  	}).get();
-
-		var maxHeight = Math.max.apply(null, elementHeights);
-
-		 $('.testimonials .col').height(maxHeight + 50); 
-	}
-
-	fixHeight();
-
-	// Fix Image Height
-	function fixImgHeight(){
-		var elementHeights = $('.product-image').map(function() {
-	    	return $(this).height();
-	  	}).get();
-
-		var maxHeight = Math.max.apply(null, elementHeights);
-		var imgPadding = maxHeight/4
-		console.log(maxHeight/4)
-		
-		$('.product-image.short').css({
-			'padding-top': imgPadding - 20,
-			'padding-bottom': imgPadding -30
-
-		}); 
-	}
-
-	fixImgHeight();
-
-	$( window ).resize(function() { 
-		fixHeight();
-		fixImgHeight();
-		fixTrioHeight()
-	})
-
-	$('.displayTableData').on('click', function(){
-		$('.product-table').toggleClass('active')
-	})
-
+	
+	$('.intro-animate').removeClass('start').delay(800).queue(function(next){
+	    $(this).addClass("end");
+	    next();
+	});
 })
