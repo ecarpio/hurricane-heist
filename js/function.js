c1d456c8ad8c96542c40a4ed5979e0880a495708
Ed <carpio.ed@gmail.com>
2018-01-21 23:49:16 -0800

    continue building site

--- js/function.js
+++ js/function.js
@@ -1,22 +1,51 @@
 $(function(){
 	console.log('test')
 	
-	// Animate website components in
-	$('.intro-animate').removeClass('start').delay(800).queue(function(next){
-	    $(this).addClass('end');
-	    $('#home-background').get(0).play();
-	    next();
+	function startIntro() {
+
+		// Animate website components in
+		$('.intro-animate').removeClass('start').delay(800).queue(function(next){
+		    $(this).addClass('end');
+		    $('#home-background').get(0).play();
+		    $('.footer-overlay').removeClass('hide')
+		    next();
+		});
+
+
+		// Show footer overlay and hide
+		$('.footer-overlay').delay(4000).queue(function(hideFooter){
+			$(this).addClass('hide')
+			hideFooter();
+		})
+
+	}	
+
+
+	$('#movieTrailerModal').modal('show');
+
+	$('#movieTrailer').on('ended',function(){
+      $('#movieTrailerModal').modal('hide');
+      startIntro()
+  });
+
+
+	// Close Menu
+
+	function closeMenu() {
+		$('.navbar-collapse').removeClass('show')
+	}
+
+	$( window ).resize(function() {
+	  closeMenu();
 	});
 
 
-	// Show footer overlay and hide
-	$('.footer-overlay').delay(5000).queue(function(hideFooter){
-		$(this).addClass('hide')
-		hideFooter();
-	})
+
+	
 
 	// Main Navigation
 	$('.main-nav a').on('click', function(){
+		closeMenu();
 		$('.main-nav a').removeClass('active');
 		$('.sub-page').removeClass('active');
 		$(this).addClass('active');
@@ -26,7 +55,7 @@ $(function(){
 			var activePage = $(this).data('nav');
 			$('.sub-page.'+ activePage).addClass('active');
 				
-		} else if (!$(this).hasClass('active')) {
+		} else if (!$('.sub-page').hasClass('active')) {
 			$('#home-background').get(0).play();
 		}
 
