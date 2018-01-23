f6ab7a7b38280e026cf858aa33045511e4bf28ca
Ed <ed.carpio@nafinc.com>
2018-01-23 14:58:23 -0800

    building site

--- js/function.js
+++ js/function.js
@@ -1,8 +1,8 @@
 $(function(){
 	console.log('test')
 	
+	// Start Intro Animations
 	function startIntro() {
-
 		// Animate website components in
 		$('.intro-animate').removeClass('start').delay(800).queue(function(next){
 		    $(this).addClass('end');
@@ -11,26 +11,50 @@ $(function(){
 		    next();
 		});
 
-
 		// Show footer overlay and hide
 		$('.footer-overlay').delay(4000).queue(function(hideFooter){
 			$(this).addClass('hide')
 			hideFooter();
 		})
-
 	}	
 
-
+	// Start Trailer Logo Animations
+	function trailerAnimate() {
+		$('.trailer-logo').removeClass('start').delay(800).queue(function(next){
+		    $(this).addClass('end');
+		    next();
+		});
+	}
+		// Spawn Movie Modal
 	$('#movieTrailerModal').modal('show');
 
+	// Play Trailer on Modal Show
+	$('#movieTrailerModal').on('shown.bs.modal', function () {
+		console.log('show')
+		trailerAnimate()
+	    $('#movieTrailer').get(0).play();
+	});
+
+	// On Trailer End
 	$('#movieTrailer').on('ended',function(){
       $('#movieTrailerModal').modal('hide');
       startIntro()
-  });
+  	});
+
+	// On Trailer Modal Close
+	$("#movieTrailerModal").on('hidden.bs.modal', function () {
+		$('.trailer-logo').removeClass('end').addClass('start');
+		$('#movieTrailer').get(0).load();
+	    startIntro()
+	});
+
+	// Watch Trailer Button
+	$('#watchTrailer').on('click', function(){
+		$('#movieTrailerModal').modal('show');
+	})
 
 
 	// Close Menu
-
 	function closeMenu() {
 		$('.navbar-collapse').removeClass('show')
 	}
@@ -55,11 +79,11 @@ $(function(){
 			var activePage = $(this).data('nav');
 			$('.sub-page.'+ activePage).addClass('active');
 				
-		} else if (!$('.sub-page').hasClass('active')) {
+		} 
+
+		if (!$('.sub-page').hasClass('active')) {
 			$('#home-background').get(0).play();
 		}
-
-
 	})
 
 
